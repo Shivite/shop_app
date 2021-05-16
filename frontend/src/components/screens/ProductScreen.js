0d99ef185f7ee779a9b5acfa5dbe516775f91a2d
squad-ravinder <ravinderk@squadmindsinc.com>
2021-05-16 23:50:06 +0530

    test the git update

--- frontend/src/components/screens/ProductScreen.js
+++ frontend/src/components/screens/ProductScreen.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default function ProductScreen() {
     return (
         <div>
-            ProductScreen
+            ProductScreen 
+            Crating new product screen
         </div>
     )
 }
